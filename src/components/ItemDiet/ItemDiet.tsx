7fc25abe4ce415bb244caa1dea7d9a1b5e682d44
MishynMykhailo <misham18092001@gmail.com>
2023-05-25 01:36:09 +0300

    added state

--- src/components/ItemDiet/ItemDiet.tsx
+++ src/components/ItemDiet/ItemDiet.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./ItemDiet.module.css";
 
 interface IProps {
@@ -6,14 +6,46 @@ interface IProps {
 }
 
 const ItemDiet: React.FC<IProps> = ({ date }) => {
+  const [selectId, setSelectId] = useState("");
+
+  const handleCheckboxChange = (id: string): void => {
+    setSelectId((prevState) => {
+      if (prevState === id) {
+        return "";
+      }
+      return id;
+    });
+  };
+
   return (
     <>
       {date.map(({ id, food, weight }) => (
         <li className={s.li} key={id}>
-          <input className={s.input} id={id} type="checkbox" />
-          <label htmlFor={id}>
-            <p className={s.p}>{food}</p>
-            <p className={s.p}>{weight}</p>
+          <input
+            className={s.input}
+            id={id}
+            type="checkbox"
+            onChange={() => handleCheckboxChange(id)}
+            checked={selectId === id && selectId.length > 0}
+            disabled={selectId !== id && selectId.length > 0}
+          />
+          <label
+            className={s.label}
+            htmlFor={id}
+            style={
+              selectId !== id && selectId.length > 0
+                ? {
+                    textDecoration: "line-through",
+                    opacity: "50%",
+                    textDecorationThickness: "1px",
+                  }
+                : { opacity: "100%" }
+            }
+          >
+            <p className={s.weight}>
+              {weight} {food}
+            </p>
+            <p className={s.food}></p>
           </label>
         </li>
       ))}
--- src/components/ListDiet/ListDiet.tsx
+++ src/components/ListDiet/ListDiet.tsx
@@ -2,20 +2,25 @@ import React from "react";
 import dietDate from "../../service/list.json";
 import ItemDiet from "../ItemDiet/ItemDiet";
 const SectionDiet: React.FC = () => {
-  console.log(dietDate.firstDiet.oneSection);
   return (
     <div>
       <ul>
         <ItemDiet date={dietDate.firstDiet.oneSection} />
+        <hr />
         <ItemDiet date={dietDate.firstDiet.twoSection} />
+        <hr />
         <ItemDiet date={dietDate.firstDiet.threeSection} />
       </ul>
+      <hr />
+
       <ul>
         <ItemDiet date={dietDate.twoDiet.oneSection} />
         <ItemDiet date={dietDate.twoDiet.twoSection} />
         <ItemDiet date={dietDate.twoDiet.threeSection} />
         <ItemDiet date={dietDate.twoDiet.fourSection} />
       </ul>
+      <hr />
+
       <ul>
         <ItemDiet date={dietDate.threeDiet.oneSection} />
         <ItemDiet date={dietDate.threeDiet.twoSection} />
